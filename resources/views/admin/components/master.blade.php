b2f1d0e87923ee02bce62cef66b7f5791345b65b
DeveloperZahin <abuzahinmohammadnowsin@gmail.com>
2024-03-19 14:30:02 +0600

    Admin Dashboard set up

---
+++ resources/views/admin/components/master.blade.php
@@ -0,0 +1,74 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--favicon-->
+    <link rel="icon" href="{{ asset('adminbackend/assets/images/favicon-32x32.png') }}" type="image/png" />
+    <!--plugins-->
+    <link href="{{ asset('adminbackend/assets/plugins/vectormap/jquery-jvectormap-2.0.2.css') }}" rel="stylesheet"/>
+    <link href="{{ asset('adminbackend/assets/plugins/simplebar/css/simplebar.css') }}" rel="stylesheet" />
+    <link href="{{ asset('adminbackend/assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css') }}" rel="stylesheet" />
+    <link href="{{ asset('adminbackend/assets/plugins/metismenu/css/metisMenu.min.css') }}" rel="stylesheet" />
+    <!-- loader-->
+    <link href="{{ asset('adminbackend/assets/css/pace.min.css') }}" rel="stylesheet" />
+    <script src="{{ asset('adminbackend/assets/js/pace.min.js') }}"></script>
+    <!-- Bootstrap CSS -->
+    <link href="{{ asset('adminbackend/assets/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('adminbackend/assets/css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('adminbackend/assets/css/icons.css') }}" rel="stylesheet">
+    <!-- Theme Style CSS -->
+    <link rel="stylesheet" href="{{ asset('adminbackend/assets/css/dark-theme.css') }}" />
+    <link rel="stylesheet" href="{{ asset('adminbackend/assets/css/semi-dark.css') }}" />
+    <link rel="stylesheet" href="{{ asset('adminbackend/assets/css/header-colors.css') }}" />
+    <title>Rukada - Responsive Bootstrap 5 Admin Template</title>
+</head>
+
+<body>
+    <!--wrapper-->
+    <div class="wrapper">
+
+        @include('admin.components.sidebar')
+        @include('admin.components.header')
+        @yield('content')
+
+        <!--start overlay-->
+        <div class="overlay toggle-icon"></div>
+        <!--end overlay-->
+        <!--Start Back To Top Button--> <a href="javaScript:;" class="back-to-top"><i class='bx bxs-up-arrow-alt'></i></a>
+        <!--End Back To Top Button-->
+
+        @include('admin.components.footer')
+        
+    </div>
+    <!--end wrapper-->
+
+    @include('admin.components.switch')
+
+    <!-- Bootstrap JS -->
+    <script src="{{ asset('adminbackend/assets/js/bootstrap.bundle.min.js') }}"></script>
+    <!--plugins-->
+    <script src="{{ asset('adminbackend/assets/js/jquery.min.js') }}"></script>
+    <script src="{{ asset('adminbackend/assets/plugins/simplebar/js/simplebar.min.js') }}"></script>
+    <script src="{{ asset('adminbackend/assets/plugins/metismenu/js/metisMenu.min.js') }}"></script>
+    <script src="{{ asset('adminbackend/assets/plugins/perfect-scrollbar/js/perfect-scrollbar.js') }}"></script>
+    <script src="{{ asset('adminbackend/assets/plugins/chartjs/js/Chart.min.js') }}"></script>
+    <script src="{{ asset('adminbackend/assets/plugins/vectormap/jquery-jvectormap-2.0.2.min.js') }}"></script>
+    <script src="{{ asset('adminbackend/assets/plugins/vectormap/jquery-jvectormap-world-mill-en.js') }}"></script>
+    <script src="{{ asset('adminbackend/assets/plugins/jquery.easy-pie-chart/jquery.easypiechart.min.js') }}"></script>
+    <script src="{{ asset('adminbackend/assets/plugins/sparkline-charts/jquery.sparkline.min.js') }}"></script>
+    <script src="{{ asset('adminbackend/assets/plugins/jquery-knob/excanvas.js') }}"></script>
+    <script src="{{ asset('adminbackend/assets/plugins/jquery-knob/jquery.knob.js') }}"></script>
+      <script>
+          $(function() {
+              $(".knob").knob();
+          });
+      </script>
+      <script src="{{ asset('adminbackend/assets/js/index.js') }}"></script>
+    <!--app JS-->
+    <script src="{{ asset('adminbackend/assets/js/app.js') }}"></script>
+</body>
+
+</html>
