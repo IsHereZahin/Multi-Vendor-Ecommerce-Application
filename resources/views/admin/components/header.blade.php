5d71df242ccf63d7045919e292bf48f281cbb6e5
DeveloperZahin <abuzahinmohammadnowsin@gmail.com>
2024-03-20 13:23:39 +0600

    Customize Dashboard

--- resources/views/admin/components/header.blade.php
+++ resources/views/admin/components/header.blade.php
@@ -417,10 +417,11 @@
 
             <div class="user-box dropdown">
                 <a class="d-flex align-items-center nav-link dropdown-toggle dropdown-toggle-nocaret" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    <img src="{{ asset('adminbackend/assets/images/avatars/avatar-2.png') }}" class="user-img" alt="user avatar">
+                    <img src="{{ asset(optional(Auth::user())->photo ? 'adminbackend/assets/images/user/' . optional(Auth::user())->photo : 'adminbackend/assets/images/no_image.jpg') }}" class="user-img" alt="User Avatar">
+
                     <div class="user-info ps-3">
-                        <p class="user-name mb-0">Pauline Seitz</p>
-                        <p class="designattion mb-0">Web Designer</p>
+                        <p class="user-name mb-0">{{ Auth::user()->name }}</p>
+                        <p class="designattion mb-0">{{ Auth::user()->email }}</p>
                     </div>
                 </a>
 
