31f08e6eb8b3acb72717f09c2bf82ba8c01a4d77
Abu Zahin Mohammad Nowsin <abuzahinmohammadnowsin@gmail.com>
2024-03-28 15:51:44 +0600

    Product add page update

--- resources/views/backend/product/product_add.blade.php
+++ resources/views/backend/product/product_add.blade.php
@@ -1,5 +1,6 @@
 @extends('admin.components.master')
 @section('content')
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <div class="page-content">
 
     <!--breadcrumb-->
@@ -61,12 +62,14 @@
 
                             <div class="mb-3">
                                 <label for="inputProductTitle" class="form-label">Main Thambnail</label>
-                                <input name="product_thambnail" class="form-control" type="file" id="formFile">
+                                <input name="product_thambnail" class="form-control" type="file" id="formFile" onChange="mainThamUrl(this)" >
+                                <img src="" id="mainThmb"/>
                             </div>
 
                             <div class="mb-3">
                                 <label for="inputProductTitle" class="form-label">Multiple Image</label>
-                                <input class="form-control" name="multi_img[]" type="file" id="formFileMultiple" multiple="">
+                                <input class="form-control" name="multi_img[]" type="file" id="multiImg" multiple="">
+                                <div class="row" id="preview_img"></div>
                             </div>
 
                         </div>
@@ -78,28 +81,28 @@
                             <div class="row g-3">
 
                                 <div class="col-md-6">
-                                    <label for="inputPrice" class="form-label">Price</label>
-                                    <input type="email" class="form-control" id="inputPrice" placeholder="00.00">
+                                    <label for="inputPrice" class="form-label">Product Price</label>
+                                    <input type="text" name="selling_price" class="form-control" id="inputPrice" placeholder="00.00">
                                 </div>
 
                                 <div class="col-md-6">
-                                    <label for="inputCompareatprice" class="form-label">Compare at Price</label>
-                                    <input type="password" class="form-control" id="inputCompareatprice" placeholder="00.00">
+                                    <label for="inputCompareatprice" class="form-label">Discount Price </label>
+                                    <input type="text" name="discount_price" class="form-control" id="inputCompareatprice" placeholder="00.00">
                                 </div>
 
                                 <div class="col-md-6">
-                                    <label for="inputCostPerPrice" class="form-label">Cost Per Price</label>
-                                    <input type="email" class="form-control" id="inputCostPerPrice" placeholder="00.00">
+                                    <label for="inputCostPerPrice" class="form-label">Product Code</label>
+                                    <input type="text" name="product_code" class="form-control" id="inputCostPerPrice" placeholder="00.00">
                                 </div>
 
                                 <div class="col-md-6">
-                                    <label for="inputStarPoints" class="form-label">Star Points</label>
-                                    <input type="password" class="form-control" id="inputStarPoints" placeholder="00.00">
+                                    <label for="inputStarPoints" class="form-label">Product Quantity</label>
+                                    <input type="text" name="product_qty" class="form-control" id="inputStarPoints" placeholder="00.00">
                                 </div>
 
                                 <div class="col-12">
-                                    <label for="inputProductType" class="form-label">Product Type</label>
-                                    <select class="form-select" id="inputProductType">
+                                    <label for="inputProductType" class="form-label">Product Brand</label>
+                                    <select name="brand_id" class="form-select" id="inputProductType">
                                         <option></option>
                                         <option value="1">One</option>
                                         <option value="2">Two</option>
@@ -108,8 +111,8 @@
                                 </div>
 
                                 <div class="col-12">
-                                    <label for="inputVendor" class="form-label">Vendor</label>
-                                    <select class="form-select" id="inputVendor">
+                                    <label for="inputVendor" class="form-label">Product Category</label>
+                                    <select name="category_id" class="form-select" id="inputVendor">
                                         <option></option>
                                         <option value="1">One</option>
                                         <option value="2">Two</option>
@@ -118,8 +121,8 @@
                                 </div>
 
                                 <div class="col-12">
-                                    <label for="inputCollection" class="form-label">Collection</label>
-                                    <select class="form-select" id="inputCollection">
+                                    <label for="inputCollection" class="form-label">Product SubCategory</label>
+					                <select name="subcategory_id" class="form-select" id="inputCollection">
                                         <option></option>
                                         <option value="1">One</option>
                                         <option value="2">Two</option>
@@ -128,9 +131,53 @@
                                 </div>
 
                                 <div class="col-12">
-                                    <label for="inputProductTags" class="form-label">Product Tags</label>
-                                    <input type="text" class="form-control" id="inputProductTags" placeholder="Enter Product Tags">
+                                    <label for="inputCollection" class="form-label">Select Vendor</label>
+                                    <select name="vendor_id" class="form-select" id="inputCollection">
+                                        <option></option>
+                                        <option value="1">One</option>
+                                        <option value="2">Two</option>
+                                        <option value="3">Three</option>
+                                      </select>
                                 </div>
+
+
+                                <div class="col-12">
+
+                                    <div class="row g-3">
+
+                                        <div class="col-md-6">
+                                            <div class="form-check">
+                                                <input class="form-check-input" name="hot_deals" type="checkbox" value="1" id="flexCheckDefault">
+                                                <label class="form-check-label" for="flexCheckDefault"> Hot Deals</label>
+                                            </div>
+                                        </div>
+
+                                        <div class="col-md-6">
+                                            <div class="form-check">
+                                                <input class="form-check-input" name="featured" type="checkbox" value="1" id="flexCheckDefault">
+                                                <label class="form-check-label" for="flexCheckDefault">Featured</label>
+                                            </div>
+                                        </div>
+
+                                        <div class="col-md-6">
+                                            <div class="form-check">
+                                                <input class="form-check-input" name="special_offer" type="checkbox" value="1" id="flexCheckDefault">
+                                                <label class="form-check-label" for="flexCheckDefault">Special Offer</label>
+                                            </div>
+                                        </div>
+
+
+                                        <div class="col-md-6">
+                                            <div class="form-check">
+                                                <input class="form-check-input" name="special_deals" type="checkbox" value="1" id="flexCheckDefault">
+                                                <label class="form-check-label" for="flexCheckDefault">Special Deals</label>
+                                            </div>
+                                        </div>
+
+                                    </div> <!-- // end row  -->
+
+                                </div>
+                                <hr>
 
                                 <div class="col-12">
                                     <div class="d-grid">
@@ -149,5 +196,43 @@
         </div>
     </div>
 </div>
+<script type="text/javascript">
+	function mainThamUrl(input){
+		if (input.files && input.files[0]) {
+			var reader = new FileReader();
+			reader.onload = function(e){
+				$('#mainThmb').attr('src',e.target.result).width(80).height(80);
+			};
+			reader.readAsDataURL(input.files[0]);
+		}
+	}
+</script>
 
+<script>
+  $(document).ready(function(){
+   $('#multiImg').on('change', function(){ //on file input change
+      if (window.File && window.FileReader && window.FileList && window.Blob) //check File API supported browser
+      {
+          var data = $(this)[0].files; //this file data
+
+          $.each(data, function(index, file){ //loop though each file
+              if(/(\.|\/)(gif|jpe?g|png)$/i.test(file.type)){ //check supported file type
+                  var fRead = new FileReader(); //new filereader
+                  fRead.onload = (function(file){ //trigger function on successful read
+                  return function(e) {
+                      var img = $('<img/>').addClass('thumb').attr('src', e.target.result) .width(100)
+                  .height(80); //create image element
+                      $('#preview_img').append(img); //append image to output element
+                  };
+                  })(file);
+                  fRead.readAsDataURL(file); //URL representing the file's data.
+              }
+          });
+
+      }else{
+          alert("Your browser doesn't support File API!"); //if File API is absent
+      }
+   });
+  });
+</script>
 @endsection
