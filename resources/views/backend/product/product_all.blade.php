4c9eab0fa9176084d44f32b6359615c1d7ed52af
Abu Zahin Mohammad Nowsin <abuzahinmohammadnowsin@gmail.com>
2024-03-29 13:03:08 +0600

    All product page update

--- resources/views/backend/product/product_all.blade.php
+++ resources/views/backend/product/product_all.blade.php
@@ -3,19 +3,19 @@
 <div class="page-content">
     <!--breadcrumb-->
     <div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
-        <div class="breadcrumb-title pe-3">All Product</div>
+        <div class="breadcrumb-title pe-3">Product</div>
         <div class="ps-3">
             <nav aria-label="breadcrumb">
                 <ol class="breadcrumb mb-0 p-0">
-                    <li class="breadcrumb-item"><a href="javascript:;"><i class="bx bx-home-alt"></i></a>
+                    <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}"><i class="bx bx-home-alt"></i></a>
                     </li>
-                    <li class="{{ route('add.product') }}" aria-current="page">All Product</li>
+                    <li class="breadcrumb-item" aria-current="page">All Products (<span style="color: rgb(0, 119, 255);">{{ count($products) }}</span>)</li>
                 </ol>
             </nav>
         </div>
         <div class="ms-auto">
             <div class="btn-group">
-                <a href="{{ route('add.category') }}" class="btn btn-primary">Add Product</a>
+                <a href="{{ route('add.product') }}" class="btn btn-primary">Add Product</a>
             </div>
         </div>
     </div>
@@ -32,8 +32,8 @@
                             <th>Image </th>
                             <th>Product Name </th>
                             <th>Price </th>
-                            <th>QTY </th>
                             <th>Discount </th>
+                            <th>QTY </th>
                             <th>Status </th>
                             <th>Action</th>
                         </tr>
@@ -45,9 +45,25 @@
                             <td> <img src="{{ asset($item->product_thambnail) }}" style="width: 70px; height:40px;" >  </td>
                             <td>{{ $item->product_name }}</td>
                             <td>{{ $item->selling_price }}</td>
+                            <td>
+                                @if($item->discount_price == NULL)
+                                    <span class="badge rounded-pill bg-info">No Discount</span>
+                                @else
+                                    <span class="badge rounded-pill bg-danger">{{ ($item->discount_price / $item->selling_price) * 100 }}%</span>
+                                @endif
+                            </td>
                             <td>{{ $item->product_qty }}</td>
-                            <td>{{ $item->discount_price }}</td>
-                            <td>{{ $item->status }}</td>
+                            <td>
+                                @if($item->status == 1)
+                                    <a href="">
+                                        <span class="badge bg-success">Active</span>
+                                    </a>
+                                @else
+                                    <a href="">
+                                        <span class="badge bg-danger">InActive</span>
+                                    </a>
+                                @endif
+                            </td>
                             <td>
                                 <a href="{{ route('edit.category',$item->id) }}" class="btn btn-info">Edit</a>
                                 <a href="{{ route('delete.category',$item->id) }}" class="btn btn-danger" id="delete" >Delete</a>
