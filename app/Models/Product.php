122b4260d078cd12c6e30dddc4dbc08ec40d000e
Abu Zahin Mohammad Nowsin <abuzahinmohammadnowsin@gmail.com>
2024-04-04 12:52:25 +0600

    New product setup on frontend

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,4 +9,24 @@ class Product extends Model
 {
     use HasFactory;
     protected $guarded = [];
+
+    public function multiImages()
+    {
+        return $this->hasMany(MultiImg::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function vendor()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function brand()
+    {
+        return $this->belongsTo(Brand::class);
+    }
 }
--- resources/views/frontend/components/quick_view.blade.php
+++ resources/views/frontend/components/quick_view.blade.php
@@ -11,25 +11,25 @@
                                 <!-- MAIN SLIDES -->
                                 <div class="product-image-slider">
                                     <figure class="border-radius-10">
-                                        <img src="{{ asset('frontend/assets/imgs/shop/product-16-2.jpg') }}" alt="product image" />
+                                        <img src="{{ asset('frontend/assets/imgs/shop/thumbnail-3.jpg') }}" alt="product image" />
                                     </figure>
                                     <figure class="border-radius-10">
-                                        <img src="{{ asset('frontend/assets/imgs/shop/product-16-1.jpg') }}" alt="product image" />
+                                        <img src="{{ asset('frontend/assets/imgs/shop/thumbnail-4.jpg') }}" alt="product image" />
                                     </figure>
                                     <figure class="border-radius-10">
-                                        <img src="{{ asset('frontend/assets/imgs/shop/product-16-3.jpg') }}" alt="product image" />
+                                        <img src="{{ asset('frontend/assets/imgs/shop/thumbnail-5.jpg') }}" alt="product image" />
                                     </figure>
                                     <figure class="border-radius-10">
-                                        <img src="{{ asset('frontend/assets/imgs/shop/product-16-4.jpg') }}" alt="product image" />
+                                        <img src="{{ asset('frontend/assets/imgs/shop/thumbnail-6.jpg') }}" alt="product image" />
                                     </figure>
                                     <figure class="border-radius-10">
-                                        <img src="{{ asset('frontend/assets/imgs/shop/product-16-5.jpg') }}" alt="product image" />
+                                        <img src="{{ asset('frontend/assets/imgs/shop/thumbnail-7.jpg') }}" alt="product image" />
                                     </figure>
                                     <figure class="border-radius-10">
-                                        <img src="{{ asset('frontend/assets/imgs/shop/product-16-6.jpg') }}" alt="product image" />
+                                        <img src="{{ asset('frontend/assets/imgs/shop/thumbnail-8.jpg') }}" alt="product image" />
                                     </figure>
                                     <figure class="border-radius-10">
-                                        <img src="{{ asset('frontend/assets/imgs/shop/product-16-7.jpg') }}" alt="product image" />
+                                        <img src="{{ asset('frontend/assets/imgs/shop/thumbnail-9.jpg') }}" alt="product image" />
                                     </figure>
                                 </div>
                                 <!-- THUMBNAILS -->
