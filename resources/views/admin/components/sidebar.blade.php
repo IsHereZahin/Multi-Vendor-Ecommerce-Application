cf51ecd49cdd9de7c8279fc3a0ff14f895c08a69
isherezahin <abuzahinmohammadnowsin@gmail.com>
2024-07-12 04:23:06 -0700

    updated

--- resources/views/admin/components/sidebar.blade.php
+++ resources/views/admin/components/sidebar.blade.php
@@ -5,7 +5,7 @@
 					<img src="{{ asset('adminbackend/assets/images/logo-icon.png') }}" class="logo-icon" alt="logo icon">
 				</div>
 				<div>
-					<h4 class="logo-text">Admin</h4>
+					<h4 class="logo-text"> <a href="{{ route('admin.dashboard') }}">Admin</a></h4>
 				</div>
 				<div class="toggle-icon ms-auto"><i class='bx bx-arrow-to-left'></i>
 				</div>
